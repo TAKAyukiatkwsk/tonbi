19e6bc34575942fbf68dd5b3645d2a61b9fc2a35
TAKAyukiatkwsk <takagi.takayuki.yuuki@gmail.com>
2013-02-07 17:53:03 +0900

    Request request token to Twitter

--- javascripts/twitter.js
+++ javascripts/twitter.js
@@ -11,5 +11,15 @@ Twitter.prototype.userLogin = function () {
     consumerSecret: CONSUMER_SECRET
   };
   var oauth = OAuth(options);
+
+  oauth.get('https://api.twitter.com/oauth/request_token',
+      function (data) {
+        // succeed to get request token
+        console.log(data);
+      },
+      function (data) {
+        // fail to get request token
+        console.log(data);
+      });
 }
 
